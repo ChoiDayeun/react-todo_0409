9a96ceeec6172fff919891bbada56570a35921a8
Dayeun Choi <120041848+ChoiDayeun@users.noreply.github.com>
2024-04-30 12:08:34 +0900

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBbgHL5Imt0NXsDArzYZk3GOCff0MJxt5I",
-  authDomain: "todo-wolf.firebaseapp.com",
-  projectId: "todo-wolf",
-  storageBucket: "todo-wolf.appspot.com",
-  messagingSenderId: "140418682685",
-  appId: "1:140418682685:web:4b609bcb360213cf5abf54",
-  measurementId: "G-GGJ1HFE6GJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
